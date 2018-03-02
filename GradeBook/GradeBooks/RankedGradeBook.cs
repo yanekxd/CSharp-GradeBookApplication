dc045944dfe8a781d3a6363150b1f15768735b4f
yanekxd <skarzynski.jan@gmail.com>
2018-03-02 17:10:07 +0100

    GetLetterGrade for RankedGradeBook
    overrided GetLetterGrade method for RankedGradeBook class

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -7,5 +7,21 @@ namespace GradeBook.GradeBooks
         {
             Type = GradeBookType.Ranked;
         }
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (Students.Count < 5)
+                throw new System.InvalidOperationException("Ranked Grading requires a minimum of 5 students to work");
+           else if (averageGrade >= 80)
+                return 'A';
+            else if (averageGrade >= 60)
+                return 'B';
+            else if (averageGrade >= 40)
+                return 'C';
+            else if (averageGrade >= 20)
+                return 'D';
+            else
+                return 'F';
+        }
     }
 }
