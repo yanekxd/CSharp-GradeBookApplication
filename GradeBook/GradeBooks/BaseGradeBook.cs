1a822960c87ccd2446a7fccace9a8dda03caeeeb
yanekxd <skarzynski.jan@gmail.com>
2018-04-03 16:27:58 +0200

    add support for weighted grading 1-4

--- GradeBook/GradeBooks/BaseGradeBook.cs
+++ GradeBook/GradeBooks/BaseGradeBook.cs
@@ -14,10 +14,12 @@ namespace GradeBook.GradeBooks
         public GradeBookType Type { get; set; }
         public string Name { get; set; }
         public List<Student> Students { get; set; }
+        public bool IsWeighted { get; set; }
 
-        public BaseGradeBook(string name)
+        public BaseGradeBook(string name, bool isWeighted)
         {
             Name = name;
+            IsWeighted = isWeighted;
             Students = new List<Student>();
         }
 
--- GradeBook/UserInterfaces/StartingUserInterface.cs
+++ GradeBook/UserInterfaces/StartingUserInterface.cs
@@ -33,21 +33,22 @@ namespace GradeBook.UserInterfaces
         public static void CreateCommand(string command)
         {
             var parts = command.Split(' ');
-            if (parts.Length != 3)
+            if (parts.Length != 4)
             {
-                Console.WriteLine("Command not valid, Create requires a name and type of gradebook.");
+                Console.WriteLine("Command not valid, Create requires a name, type of gradebook, if it's weighted (true / false).");
                 return;
             }
             var name = parts[1];
             var type = parts[2].ToLower();
+            var isWeighted = bool.Parse(parts[3]);
             BaseGradeBook gradeBook;
             if (type == "standard")
             {
-                gradeBook = new StandardGradeBook(name);
+                gradeBook = new StandardGradeBook(name, isWeighted);
             }
             else if (type == "ranked")
             {
-                gradeBook = new RankedGradeBook(name);
+                gradeBook = new RankedGradeBook(name, isWeighted);
             }
             else
             {
